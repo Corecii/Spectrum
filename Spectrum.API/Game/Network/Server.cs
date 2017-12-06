52d45cd3610ffb1b2b3fc9e81b58160efc5ff655
Tomasz Cichoń <t.cichon.64@outlook.com>
2017-12-06 09:42:49 +0100

    Removed buggy Create() method.

--- Spectrum.API/Game/Network/Server.cs
+++ Spectrum.API/Game/Network/Server.cs
@@ -51,34 +51,5 @@ namespace Spectrum.API.Game.Network
                 }
             });
         }
-
-        public static void Create(string serverTitle, string password, int maxPlayerCount)
-        {
-            UnityEngine.Network.InitializeSecurity();
-            try
-            {
-                G.Sys.NetworkingManager_.password_ = password;
-                G.Sys.NetworkingManager_.serverTitle_ = serverTitle;
-                G.Sys.NetworkingManager_.maxPlayerCount_ = maxPlayerCount;
-
-                G.Sys.GameData_.SetString("ServerTitleDefault", serverTitle);
-                G.Sys.GameData_.SetInt("MaxPlayersDefault", maxPlayerCount);
-
-                var ncError = UnityEngine.Network.InitializeServer(maxPlayerCount - 1, 32323, true);
-
-                if (ncError == NetworkConnectionError.NoError)
-                {
-                    var eventArgs = new ServerCreatedEventArgs(serverTitle, password, maxPlayerCount);
-                    ServerCreated?.Invoke(null, eventArgs);
-                    return;
-                }
-
-                G.Sys.MenuPanelManager_.ShowError(ncError.ToString(), "Failed to start server");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex.Message);
-            }
-        }
     }
 }
