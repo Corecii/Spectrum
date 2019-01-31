ed3d25c85d94093dd041e05df773dec71852c5b9
Tomasz Cichoń <mrciastexx@gmail.com>
2019-01-31 09:00:34 +0100

    Initial commit for the Resonator Mod Manager.

---
+++ Spectrum.Resonator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System.Windows;
+
+namespace Spectrum.Resonator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string TestBindableString { get; set; } = "This is a test.";
+
+        [Command]
+        public void ClickityClick()
+        {
+            MessageBox.Show("Yay.");
+        }
+    }
+}
